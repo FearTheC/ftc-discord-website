cf518ae54740b9b1cd1e042cdac24a7cbd974f01
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-08-10 12:18:14 +0200

    Updates WebAdmin Hompage factory according to previous commit

--- src/WebAdmin/src/Container/Handler/HomePageFactory.php
+++ src/WebAdmin/src/Container/Handler/HomePageFactory.php
@@ -6,23 +6,15 @@ namespace FTC\WebAdmin\Container\Handler;
 
 use Psr\Container\ContainerInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Expressive\Router\RouterInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
-use FTC\Trello\Client;
-use FTC\Discord\Model\Aggregate\GuildMemberRepository;
-use FTC\Discord\Model\Aggregate\GuildRoleRepository;
 use FTC\WebAdmin\Handler\HomePage;
 
 class HomePageFactory
 {
     public function __invoke(ContainerInterface $container) : RequestHandlerInterface
     {
-        $router   = $container->get(RouterInterface::class);
         $template = $container->get(TemplateRendererInterface::class);
-        $guildMemberRepo = $container->get(GuildMemberRepository::class);
-        $guildRolesRepo = $container->get(GuildRoleRepository::class);
-        $trelloClient = $container->get(Client::class);
 
-        return new HomePage($router, $template, $guildMemberRepo, $guildRolesRepo, $trelloClient);
+        return new HomePage($template);
     }
 }
